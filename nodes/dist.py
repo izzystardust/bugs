271b5084166e54d327440a0bc84a68d77d0a56af
Isabelle Miller <izzy@stardust.fm>
2015-02-22 18:21:36 -0500

    Shit's broken, yo

--- nodes/dist.py
+++ nodes/dist.py
@@ -6,15 +6,15 @@ class Dist:
         self.m = threading.Lock()
         self.left = 0
         self.front = 0
+        self.raw = []
 
-    def update(self, vals, range_min, range_max):
+    def update(self, data):
         # these magic numbers were acquired from Alan Beadle
         # straight ahead is 540, 40 index range should be enough
         # left chosen to look slightly back to get in front of wall before turning
-
         def getmin(a, b):
-            in_rng = lambda x: range_min <= x <= range_max
-            vsp = filter(in_rng, vals[a:b])
+            in_rng = lambda x: data.range_min <= x <= data.range_max
+            vsp = filter(in_rng, data.ranges[a:b])
             if len(vsp) > 0:
                 return min(vsp)
             else:
@@ -26,6 +26,7 @@ class Dist:
         self.m.acquire()
         self.left = newleft
         self.front = newfront
+        self.raw = data
         self.m.release()
 
     def get(self):
@@ -34,3 +35,29 @@ class Dist:
         f = self.front
         self.m.release()
         return (f, l)
+
+    def angle_to_index(self, angle):
+        return int((angle - self.raw.angle_min)/self.raw.angle_increment)
+
+    # angle in radians
+    def at(self, angle):
+        # TODO(exm): copy and paste programming, refactor later
+        def getmin(a, b):
+            in_rng = lambda x: self.raw.range_min <= x <= self.raw.range_max
+            vsp = filter(in_rng, self.raw.ranges[a:b])
+            if len(vsp) > 0:
+                return min(vsp)
+            else:
+                return sys.maxint
+        self.m.acquire()
+        i = self.angle_to_index(angle)
+        print "Using index", i
+        start = i - 40
+        if start < 0:
+            start = 0
+        end = i + 40
+        if end >= len(self.raw.ranges):
+            end = len(self.raw.ranges) - 1
+        ans = getmin(start, end)
+        self.m.release()
+        return ans
