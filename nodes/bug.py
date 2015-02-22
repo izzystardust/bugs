97328308415b06a6341d869a64bb121fd4a66248
Isabelle Miller <izzy@stardust.fm>
2015-02-22 16:06:22 -0500

    Implementation correct as long as no obstacles
    Added sensor readings as well

--- nodes/bug.py
+++ nodes/bug.py
@@ -3,7 +3,7 @@
 import sys
 import roslib; roslib.load_manifest('bugs')
 import rospy
-import tf.transformations
+import tf.transformations as transform
 import pycsp.greenlets as csp
 
 from geometry_msgs.msg import Twist
@@ -11,49 +11,53 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 
 from location import Location
+from dist import Dist
 
 current_location = Location()
+current_dists = Dist()
 
-delta = 1.0
+delta = .1
 
 GO_ST = 0
-GO_RT = 1
-GO_LF = 2
+GO_RT = 2
 MSG_STOP = 3
 
 
 def init_listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber('base_pose_ground_truth', Odometry, location_callback)
+    rospy.Subscriber('base_scan', LaserScan, sensor_callback)
 
 def location_callback(data):
     p = data.pose.pose.position
-    q = data.pose.pose.orientation
-    t = transformations.euler_from_quaternion(q)
-    current_location.update_location(p.x, p.y, q)
-    #print data.pose.pose.orientation
+    q = (
+            data.pose.pose.orientation.x,
+            data.pose.pose.orientation.y,
+            data.pose.pose.orientation.z,
+            data.pose.pose.orientation.w)
+    t = transform.euler_from_quaternion(q)[2] # in [-pi, pi]
+    current_location.update_location(p.x, p.y, t)
+
+def sensor_callback(data):
+    current_dists.update(data.ranges, data.range_min, data.range_max)
 
 @csp.process
 def mover(cin):
-    pub = rospy.Publisher('cmd_vel', Twist)
-    rospy.init_node('buggy')
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
     cmd = Twist()
     while True:
         c, got = csp.AltSelect(
                 csp.InputGuard(cin),
-                csp.TimeoutGuard(seconds=0.01)
-                )
+                csp.TimeoutGuard(seconds=0.01))
+
         if c != cin:
             pass
         elif got == GO_ST:
             cmd = Twist()
             cmd.linear.x = 1
-        elif got == GO_LF:
-            cmd = Twist()
-            cmd.angular.z = 0.5
         elif got == GO_RT:
             cmd = Twist()
-            cmd.angular.z = -0.5
+            cmd.angular.z = -0.25
         elif got == MSG_STOP:
             cmd = Twist()
             pub.publish(cmd)
@@ -65,17 +69,29 @@ def mover(cin):
 def bug_algorithm(out):
     init_listener()
     while current_location.distance(tx, ty) > delta:
-        out(GO_ST)
+        print "At", current_location.current_location()
+        print "Sensors", current_dists.get()
+        if not current_location.facing_point(tx, ty):
+            out(GO_RT)
+        else:
+            out(GO_ST)
         rospy.sleep(.01)
+    out(MSG_STOP)
 
+
+# Parse arguments
 algorithm = sys.argv[1]
 algorithms = ["bug0", "bug1", "bug2"]
 if algorithm not in algorithms:
     print "First argument should be one of ", algorithms, ". Was ", algorithm
     sys.exit(1)
 
+if len(sys.argv) < 4:
+    print "Usage: rosrun bugs bug.py ALGORITHM X Y"
+    sys.exit(1)
 (tx, ty) = map(float, sys.argv[2:4])
 print "Setting target: (", tx, ", ", ty, ")"
+
 
 C = csp.Channel()
 csp.Parallel(
@@ -84,6 +100,5 @@ csp.Parallel(
 )
 
 
-print "Not there yet (", current_location.distance(tx, ty), ")"
 print "There: distance is ", current_location.distance(tx, ty)
 print "At", current_location.current_location()
--- nodes/location.py
+++ nodes/location.py
@@ -2,33 +2,47 @@ import threading
 import math
 import sys
 
+# Location is used to maintain a single current location of the robot in a
+# thread-safe manner such that the callback and readers can all use it without
+# issue
 class Location:
     def __init__(self):
-        self.m = threading.Lock()
+        self.m = threading.Lock() # global lock b/c easy and not a problem yet
         self.x = None
         self.y = None
-	self.t = None
+        self.t = None
+        self.deltaT = 0.05 # how close to angle to be to go
 
     def update_location(self, x, y, t):
         self.m.acquire()
         self.x = x
         self.y = y
-	self.t = t
+        self.t = t
         self.m.release()
 
     def current_location(self):
         self.m.acquire()
         x = self.x
         y = self.y
-	t = self.t
+        t = self.t
         self.m.release()
         return (x, y, t)
 
     def distance(self, x, y):
         (x0, y0, _) = self.current_location()
         if x0 == None or y0 == None:
+            # will be none on the first iteration
             return sys.maxint
         return math.sqrt((x-x0)**2 + (y-y0)**2)
 
-    def required_heading(self, x, y):
-            pass
+    def facing_point(self, x, y):
+        (cx, cy, current_heading) = self.current_location()
+        if None in (cx, cy, current_heading):
+            return False
+        n = necessary_heading(cx, cy, x, y)
+        # TODO(exm) possible bug with boundary conditions?
+        return n - self.deltaT <= current_heading <= n + self.deltaT
+
+# current x, y; target x,y
+def necessary_heading(cx, cy, tx, ty):
+    return math.atan2(ty-cy, tx-cx)
