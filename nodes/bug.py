10b9bdbd6fdeedbc7ad4e251f19d732cc3444b5d
Ethan Miller <millere@clarkson.edu>
2015-02-22 21:58:01 -0500

    "Working"

--- nodes/bug.py
+++ nodes/bug.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
+import math
 import sys
 import roslib; roslib.load_manifest('bugs')
 import rospy
 import tf.transformations as transform
-import pycsp.greenlets as csp
 
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
@@ -19,11 +19,10 @@ current_dists = Dist()
 delta = .1
 WALL_PADDING = .5
 
-GO_ST = 0
-GO_LF = 1
-GO_RT = 2
+STRAIGHT = 0
+LEFT = 1
+RIGHT = 2
 MSG_STOP = 3
-
 
 def init_listener():
     rospy.init_node('listener', anonymous=True)
@@ -43,35 +42,154 @@ def location_callback(data):
 def sensor_callback(data):
     current_dists.update(data)
 
-@csp.process
-def mover(cin):
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-    cmd = Twist()
-    while True:
-        c, got = csp.AltSelect(
-                csp.InputGuard(cin),
-                csp.TimeoutGuard(seconds=0.01))
+class Bug:
+    def __init__(self, tx, ty):
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.tx = tx
+        self.ty = ty
 
-        if c != cin:
-            pass
-        elif got == GO_ST:
-            cmd = Twist()
+    def go(self, direction):
+        cmd = Twist()
+        if direction == STRAIGHT:
             cmd.linear.x = 1
-        elif got == GO_LF:
-            cmd = Twist()
+        elif direction == LEFT:
             cmd.angular.z = 0.25
-        elif got == GO_RT:
-            cmd = Twist()
+        elif direction == RIGHT:
             cmd.angular.z = -0.25
-        elif got == MSG_STOP:
-            cmd = Twist()
-            pub.publish(cmd)
-            return
+        elif direction == MSG_STOP:
+            pass
+        self.pub.publish(cmd)
 
-        pub.publish(cmd)
+    def go_until_obstacle(self):
+        print "Going until destination or obstacle"
+        while current_location.distance(tx, ty) > delta:
+            (frontdist, _) = current_dists.get()
+            if frontdist <= WALL_PADDING:
+                return True
 
-@csp.process
-def bug_algorithm(out):
+            if current_location.facing_point(tx, ty):
+                self.go(STRAIGHT)
+            elif current_location.faster_left(tx, ty):
+                self.go(LEFT)
+            else:
+                self.go(RIGHT)
+            rospy.sleep(.01)
+        return False
+
+    def follow_wall(self):
+        print "Following wall"
+        while current_dists.get()[0] <= WALL_PADDING:
+            self.go(RIGHT)
+            rospy.sleep(.01)
+        while not self.should_leave_wall():
+            (front, left) = current_dists.get()
+            if front <= WALL_PADDING:
+                self.go(RIGHT)
+            elif WALL_PADDING - .1 <= left <= WALL_PADDING + .1:
+                self.go(STRAIGHT)
+            elif left > WALL_PADDING + .1:
+                self.go(LEFT)
+            else:
+                self.go(RIGHT)
+            rospy.sleep(.01)
+
+    def should_leave_wall(self):
+        print "You dolt! You need to subclass bug to know how to leave the wall"
+        sys.exit(1)
+
+class Bug0(Bug):
+    def should_leave_wall(self):
+        (x, y, t) = current_location.current_location()
+        dir_to_go = current_location.global_to_local(necessary_heading(x, y, tx, ty))
+        at = current_dists.at(dir_to_go)
+        if at > 5:
+            print "Leaving wall"
+            return True
+        return False
+
+class Bug1(Bug):
+    def __init__(self, tx, ty):
+        Bug.__init__(self, tx, ty)
+        self.closest_point = (None, None)
+        self.origin = (None, None)
+        self.circumnavigated = False
+
+    def should_leave_wall(self):
+        (x, y, t) = current_location.current_location()
+
+        if None in self.closest_point:
+            self.origin = (x, y)
+            self.closest_point = (x, y)
+            self.closest_distance = current_location.distance(self.tx, self.ty)
+            self.left_origin_point = False
+            return False
+        d = current_location.distance(self.tx, self.ty)
+        if d < self.closest_distance:
+            print "New closest point at", (x, y)
+            self.closest_distance = d
+            self.closest_point = (x, y)
+
+        (ox, oy) = self.origin
+        if not self.left_origin_point and not near(x, y, ox, oy):
+            # we have now left the point where we hit the wall
+            print "Left original touch point"
+            self.left_origin_point = True
+        elif near(x, y, ox, oy) and self.left_origin_point:
+            # circumnavigation achieved!
+            print "Circumnavigated obstacle"
+            self.circumnavigated = True
+
+        (cx, ct) = self.closest_point
+        if self.circumnavigated and near(x, y, cx, ct):
+            self.closest_point = (None, None)
+            self.origin = (None, None)
+            self.circumnavigated = False
+            self.left_origin_point = False
+            print "Leaving wall"
+            return True
+
+        else:
+            return False
+
+class Bug2(Bug):
+    def __init__(self, tx, ty):
+        Bug.__init__(self, tx, ty)
+        self.lh = None
+        self.encountered_wall_at = (None, None)
+
+    def face_goal(self):
+        while not current_location.facing_point(self.tx, self.ty):
+            self.go(RIGHT)
+            rospy.sleep(.01)
+
+    def follow_wall(self):
+        Bug.follow_wall(self)
+        self.face_goal()
+
+    def should_leave_wall(self):
+        (x, y, _) = current_location.current_location()
+        if None in self.encountered_wall_at:
+            self.encountered_wall_at = (x, y)
+            self.lh = necessary_heading(x, y, self.tx, self.ty)
+            return False
+        t_angle = necessary_heading(x, y, self.tx, self.ty)
+        (ox, oy) = self.encountered_wall_at
+        od = math.sqrt((ox-self.tx)**2 + (oy-self.ty)**2)
+        cd = math.sqrt( (x-self.tx)**2 +  (y-self.ty)**2)
+        dt = 0.01
+
+        if self.lh - dt <= t_angle <= self.lh + dt and not near(x, y, ox, oy):
+            if cd < od:
+                print "Leaving wall"
+                return True
+        return False
+
+def near(cx, cy, x, y):
+    nearx = x - .3 <= cx <= x + .3
+    neary = y - .3 <= cy <= y + .3
+    return nearx and neary
+
+def bug_algorithm(bug):
     init_listener()
     print "Calibrating sensors..."
     # This actually just lets the sensor readings propagate into the system
@@ -79,53 +197,10 @@ def bug_algorithm(out):
     print "Calibrated"
 
     while current_location.distance(tx, ty) > delta:
-        hit_wall = go_until_obstacle(out)
+        hit_wall = bug.go_until_obstacle()
         if hit_wall:
-            follow_wall(out)
-    out(MSG_STOP)
+            bug.follow_wall()
     print "Arrived at", (tx, ty)
-
-def go_until_obstacle(out):
-    print "Going until destination or obstacle"
-    while current_location.distance(tx, ty) > delta:
-        (frontdist, _) = current_dists.get()
-        if frontdist <= WALL_PADDING:
-            return True
-
-        if current_location.facing_point(tx, ty):
-            out(GO_ST)
-        elif current_location.faster_left(tx, ty):
-            out(GO_LF)
-        else:
-            out(GO_RT)
-        rospy.sleep(.01)
-    return False
-
-def follow_wall(out):
-    print "Following wall"
-    while current_dists.get()[0] <= WALL_PADDING:
-        out(GO_RT)
-        rospy.sleep(.01)
-    while not should_leave_wall():
-        (front, left) = current_dists.get()
-        if front <= WALL_PADDING:
-            out(GO_RT)
-        elif WALL_PADDING - .1 <= left <= WALL_PADDING + .1:
-            out(GO_ST)
-        elif left > WALL_PADDING + .1:
-            out(GO_LF)
-        else:
-            out(GO_RT)
-        rospy.sleep(.01)
-
-def should_leave_wall():
-    (x, y, t) = current_location.current_location()
-    dir_to_go = current_location.global_to_local(necessary_heading(x, y, tx, ty))
-    at = current_dists.at(dir_to_go)
-    if at > 5:
-        print "leaving at", at
-        return True
-    return False
 
 # Parse arguments
 algorithm = sys.argv[1]
@@ -138,11 +213,16 @@ if len(sys.argv) < 4:
     print "Usage: rosrun bugs bug.py ALGORITHM X Y"
     sys.exit(1)
 (tx, ty) = map(float, sys.argv[2:4])
-print "Setting target: (", tx, ", ", ty, ")"
 
 
-C = csp.Channel()
-csp.Parallel(
-        bug_algorithm(C.writer()),
-        mover(C.reader()),
-)
+
+print "Setting target:", (tx, ty)
+bug = None
+if algorithm == "bug0":
+    bug = Bug0(tx, ty)
+elif algorithm == "bug1":
+    bug = Bug1(tx, ty)
+elif algorithm == "bug2":
+    bug = Bug2(tx, ty)
+
+bug_algorithm(bug)
