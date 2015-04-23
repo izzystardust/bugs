8a1729a2db4759fcabfd7dc761269173d9fd431a
Ethan Miller <millere@clarkson.edu>
2015-04-23 11:19:59 -0400

    Remove algorithms not relevant for HW2

--- nodes/bug.py
+++ nodes/bug.py
@@ -94,11 +94,6 @@ class Bug:
             rospy.sleep(.01)
 
     def should_leave_wall(self):
-        print "You dolt! You need to subclass bug to know how to leave the wall"
-        sys.exit(1)
-
-class Bug0(Bug):
-    def should_leave_wall(self):
         (x, y, t) = current_location.current_location()
         dir_to_go = current_location.global_to_local(necessary_heading(x, y, tx, ty))
         at = current_dists.at(dir_to_go)
@@ -107,89 +102,14 @@ class Bug0(Bug):
             return True
         return False
 
-class Bug1(Bug):
-    def __init__(self, tx, ty):
-        Bug.__init__(self, tx, ty)
-        self.closest_point = (None, None)
-        self.origin = (None, None)
-        self.circumnavigated = False
-
-    def should_leave_wall(self):
-        (x, y, t) = current_location.current_location()
-
-        if None in self.closest_point:
-            self.origin = (x, y)
-            self.closest_point = (x, y)
-            self.closest_distance = current_location.distance(self.tx, self.ty)
-            self.left_origin_point = False
-            return False
-        d = current_location.distance(self.tx, self.ty)
-        if d < self.closest_distance:
-            print "New closest point at", (x, y)
-            self.closest_distance = d
-            self.closest_point = (x, y)
-
-        (ox, oy) = self.origin
-        if not self.left_origin_point and not near(x, y, ox, oy):
-            # we have now left the point where we hit the wall
-            print "Left original touch point"
-            self.left_origin_point = True
-        elif near(x, y, ox, oy) and self.left_origin_point:
-            # circumnavigation achieved!
-            print "Circumnavigated obstacle"
-            self.circumnavigated = True
-
-        (cx, ct) = self.closest_point
-        if self.circumnavigated and near(x, y, cx, ct):
-            self.closest_point = (None, None)
-            self.origin = (None, None)
-            self.circumnavigated = False
-            self.left_origin_point = False
-            print "Leaving wall"
-            return True
-
-        else:
-            return False
-
-class Bug2(Bug):
-    def __init__(self, tx, ty):
-        Bug.__init__(self, tx, ty)
-        self.lh = None
-        self.encountered_wall_at = (None, None)
-
-    def face_goal(self):
-        while not current_location.facing_point(self.tx, self.ty):
-            self.go(RIGHT)
-            rospy.sleep(.01)
-
-    def follow_wall(self):
-        Bug.follow_wall(self)
-        self.face_goal()
-
-    def should_leave_wall(self):
-        (x, y, _) = current_location.current_location()
-        if None in self.encountered_wall_at:
-            self.encountered_wall_at = (x, y)
-            self.lh = necessary_heading(x, y, self.tx, self.ty)
-            return False
-        t_angle = necessary_heading(x, y, self.tx, self.ty)
-        (ox, oy) = self.encountered_wall_at
-        od = math.sqrt((ox-self.tx)**2 + (oy-self.ty)**2)
-        cd = math.sqrt( (x-self.tx)**2 +  (y-self.ty)**2)
-        dt = 0.01
-
-        if self.lh - dt <= t_angle <= self.lh + dt and not near(x, y, ox, oy):
-            if cd < od:
-                print "Leaving wall"
-                return True
-        return False
 
 def near(cx, cy, x, y):
     nearx = x - .3 <= cx <= x + .3
     neary = y - .3 <= cy <= y + .3
     return nearx and neary
 
-def bug_algorithm(bug):
+def bug_algorithm():
+    bug = Bug(tx, ty)
     init_listener()
     print "Calibrating sensors..."
     # This actually just lets the sensor readings propagate into the system
@@ -202,27 +122,14 @@ def bug_algorithm(bug):
             bug.follow_wall()
     print "Arrived at", (tx, ty)
 
-# Parse arguments
-algorithm = sys.argv[1]
-algorithms = ["bug0", "bug1", "bug2"]
-if algorithm not in algorithms:
-    print "First argument should be one of ", algorithms, ". Was ", algorithm
-    sys.exit(1)
 
-if len(sys.argv) < 4:
-    print "Usage: rosrun bugs bug.py ALGORITHM X Y"
+
+if len(sys.argv) < 3:
+    print "Usage: rosrun bugs bug.py X Y"
     sys.exit(1)
-(tx, ty) = map(float, sys.argv[2:4])
+(tx, ty) = map(float, sys.argv[1:3])
 
 
 
 print "Setting target:", (tx, ty)
-bug = None
-if algorithm == "bug0":
-    bug = Bug0(tx, ty)
-elif algorithm == "bug1":
-    bug = Bug1(tx, ty)
-elif algorithm == "bug2":
-    bug = Bug2(tx, ty)
-
-bug_algorithm(bug)
+bug_algorithm()
